823ef62ad90decbaee1deba72e1f0844c6d1bac6
Kaybee-debug <karabomolepo29@gmail.com>
2021-07-08 12:28:45 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-ysajuj
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ysajuj)
---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React from "react";
+import "./style.css";
+import Button from '@material-ui/core/Button'
+import CheckBox from '@material-ui/core/CheckBox'
+
+
+
+
+export default function App() {
+  return (
+    
+
+<div>
+<Button variant="contained" color="primary">
+  Primary
+</Button>
+<Button>Default</Button>
+<Button color="primary">Primary</Button>
+<Button color="secondary">Secondary</Button>
+<Button disabled>Disabled</Button>
+<Button href="#text-buttons" color="primary">
+  Link
+</Button>
+</div>
+<Button href="#text-buttons" color="primary">
+  Link
+</Button>
+
+
+
+
+
+
+     
+  );
+
+
+  }
