419c3f58dce408e02112ec53bdb8852f3a78754a
Kaybee-debug <karabomolepo29@gmail.com>
2021-07-10 16:36:50 +0200

    1 file modified

--- src/App.js
+++ src/App.js
@@ -9,7 +9,27 @@ import Grid from '@material-ui/core/Grid';
 export default function App() {
   return (
     <div>
-      <Grid container>
+      <Grid container spacing={2}> 
+      <Grid item xs={12}>
+          KARABO MALEHU MOLEPO
+        </Grid>
+
+        <Grid item xs={6}>
+        experience
+        </Grid>
+
+        <Grid item xs={6}>
+        experience
+        </Grid>
+
+        <Grid item xs={6}>
+        experience
+        </Grid>
+
+        <Grid item xs={6}>
+        experience
+        </Grid>
+
         </Grid>
     </div>
     
