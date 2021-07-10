d6dec05aae61cc59a2b195b7c7588b60534de0da
Kaybee-debug <karabomolepo29@gmail.com>
2021-07-10 17:21:04 +0200

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './style.css';
 import Grid from '@material-ui/core/Grid';
+import Container from '@material-ui/core/Container';
 
 
 
@@ -11,7 +12,29 @@ export default function App() {
     <div>
       <Grid container spacing={2}> 
       <Grid item xs={12}>
-          KARABO MALEHU MOLEPO
+      <Container width="xs" style={{backgroundColor:"orange"}} >
+          <h1>KARABO MALEHU MOLEPO</h1>
+            </Container>
+        
+          
+        </Grid>
+
+        <Grid item xs={6} container>
+          <Container width="xs" style={{backgroundColor:"orange"}}>
+          <h2>EDUCATION</h2>
+            </Container>
+           < Grid item xs={6}>
+            hello 1
+           </Grid>
+           < Grid item xs={6}>
+            hello 1.1
+           </Grid>
+           < Grid item xs={6}>
+            hello 2
+           </Grid>
+           < Grid item xs={6}>
+            hello 2.1
+           </Grid>
         </Grid>
 
         <Grid item xs={6}>
@@ -19,10 +42,11 @@ export default function App() {
         </Grid>
 
         <Grid item xs={6}>
-        experience
-        </Grid>
-
-        <Grid item xs={6}>
+        <Container width="xs" style={{backgroundColor:"orange"}}>
+          <h2>EXPERINCE</h2>
+            </Container>
+        
+          
         experience
         </Grid>
 
